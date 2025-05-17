8fc18d6365b488ca5a26b27786d991ba7e5150af
Shubham Darda <shubhamrdarda@gmail.com>
2025-05-17 23:06:45 +0530

    v25.5.1

---
+++ README.md
@@ -0,0 +1,34 @@
+## License
+Codeaft, Since 2018. The entire codebase is licensed under the [MIT LICENSE][cidzdngz].
+
+## Design Frameworks
+
+Codeaft's website versioning is maintained on [GitHub][cidezvdy] through [Git][cidzyzav], and it uses [Jekyll][cidwwvga], [Bootstrap][cidzuddz] and [jQuery][cidddcia] frameworks. 
+
+## Development and Testing Usage
+
+[![Setup Environment and Automated Tests](https://github.com/codeaft/codeaft.github.io/actions/workflows/pipeline.yml/badge.svg?branch=main)](https://github.com/codeaft/codeaft.github.io/actions/workflows/pipeline.yml)
+
+**Ubuntu Prerequisites:** Download the [setup.py][cidzgwel] file on the Linux Ubuntu machine.
+```
+Step 1: codeaft@codeaft:~$ python3 setup.py full
+Step 2: Visit the local website at http://127.0.0.1:4000 or http://localhost:4000
+```
+**Windows Prerequisites:** [Git][cidzyzav], [Visual Studio Code][cidkcqso], [Python][cidxadth] and [Ruby][cidzrvdq] are installed on the Windows machine.
+```
+Step 1: PS D:\> Open Visual Studio Code -> Terminal
+Step 2: PS D:\> git clone https://github.com/codeaft/codeaft.github.io.git
+Step 3: PS D:\codeaft.github.io> python setup.py full
+Step 4: Visit the local website at http://127.0.0.1:4000 or http://localhost:4000
+```
+
+[cidezvdy]: https://github.com
+[cidzuddz]: https://getbootstrap.com
+[cidddcia]: https://jquery.com
+[cidwwvga]: https://jekyllrb.com
+[cidzyzav]: https://git-scm.com
+[cidxadth]: https://www.python.org
+[cidzrvdq]: https://rubyinstaller.org/downloads
+[cidkcqso]: https://code.visualstudio.com
+[cidzdngz]: https://github.com/codeaft/codeaft.github.io?tab=MIT-1-ov-file
+[cidzgwel]: https://github.com/codeaft/codeaft.github.io/blob/main/setup.py
---
+++ setup.py
@@ -0,0 +1,148 @@
+"""
+Author: Shubham Darda
+Description: The Codeaft website can be set up and operated on localhost with the aid of this script.
+"""
+
+import sys
+import os
+import subprocess
+import platform
+import socket
+import shutil
+
+cwd = os.getcwd() + "/"
+codeaft = "codeaft.github.io"
+ubuntu = False
+githubactions = False
+total, used, free = shutil.disk_usage(cwd)
+freedisk = free // (2**30)
+
+def install_chrome():
+    chrome_version = os.system("google-chrome --version > /dev/null")
+    if chrome_version != 0:
+        os.system("sudo wget https://dl-ssl.google.com/linux/linux_signing_key.pub -O /tmp/google.pub")
+        os.system("sudo gpg --no-default-keyring --keyring /etc/apt/keyrings/google-chrome.gpg --import /tmp/google.pub")
+        os.system("echo 'deb [arch=amd64 signed-by=/etc/apt/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list")
+        os.system("sudo apt-get -y install google-chrome-stable")
+
+def install():
+    if ubuntu:
+        os.system("sudo apt-get update -y")
+        os.system("sudo apt-get full-upgrade -y")
+        if githubactions:
+            packages = "ruby-full clisp rustc freeglut3-dev nasm shc"
+        else:
+            packages = "python3-pip git-all openjdk-21-jre openjdk-21-jdk ruby-full build-essential zlib1g-dev dotnet-sdk-8.0 r-base octave clisp maxima rustc freeglut3-dev mysql-server nasm nmap shc finger"
+
+            snap = os.system("snap --version > /dev/null")
+            vscode = os.system("code --version > /dev/null")
+            julia = os.system("julia --version > /dev/null")
+            if snap == 0 and vscode != 0:
+                os.system("sudo snap install --classic code")
+            if snap == 0 and julia != 0:
+                os.system("sudo snap install julia --classic")
+
+        cmd = "sudo apt-get -y --ignore-missing install "
+        for pkg in packages.split():
+            command = str(cmd) + str(pkg)
+            subprocess.run(command.split())
+    
+        if not githubactions:
+            if codeaft not in os.getcwd():
+                os.system("git clone https://github.com/codeaft/codeaft.github.io.git")
+                os.chdir(cwd + codeaft + "/")
+        install_chrome()
+    
+    os.system("pip install --user --upgrade -r tests/requirements.txt --break-system-packages --no-warn-script-location")
+    if os.system("bundle config set --local path vendor/bundle") == 0:
+        os.system("bundle install")
+        os.system("bundle update --bundler")
+        os.system("bundle update")
+    else:
+        quit()
+
+def start_server():
+    try:
+        if codeaft not in os.getcwd():
+            os.chdir(cwd + codeaft + "/")
+        print("---------------------------------------------")
+        print("The following versions are getting used")
+        print("---------------------------------------------")
+        print(subprocess.check_output("ruby -v", shell=True).rstrip().decode("utf-8"))
+        print("Gem", subprocess.check_output("gem -v", shell=True).rstrip().decode("utf-8"))
+        print(subprocess.check_output("bundle exec jekyll -v", shell=True).rstrip().decode("utf-8"))
+        print(subprocess.check_output("bundler -v", shell=True).rstrip().decode("utf-8"))
+        print("---------------------------------------------")
+        if ubuntu:
+            if githubactions:
+                os.system("sudo bundle exec jekyll build")
+            else:
+                os.system("sudo bundle exec jekyll serve")
+        else:
+            os.system("bundle exec jekyll serve")
+    except KeyboardInterrupt:
+        pass
+    except:
+        print("Unable to start the Jekyll server as required resources are not found.\nRecommended to run setup.py with full as an argument to install the necessary resources.")
+
+def connected_to_internet():
+    connection = None
+    try:
+        socket.create_connection(("8.8.8.8", 53))
+        connection = True
+    except:
+        connection = False
+    finally:
+        return connection
+
+def start_setup():
+    full = False
+    try:
+        if sys.argv[1].lower() == "full" or sys.argv[1].lower() == "-full" or sys.argv[1].lower() == "--full":
+            full = True
+    except:
+        full
+    if connected_to_internet():
+        if freedisk >= 2:
+            if full:
+                if os.path.split(os.getcwd())[-1] == codeaft:
+                    with open(".gitignore", "r") as ignore_file:
+                        cleanup = ignore_file.readlines()
+                        for data in cleanup:
+                            if data.strip() != "":
+                                shutil.rmtree(data.strip(), ignore_errors=True)
+                            if ubuntu:
+                                os.system("sudo rm -rf _site/")
+                if ubuntu:
+                    install()
+                    os.system("sudo apt-get clean -y")
+                    os.system("sudo apt-get autoclean -y")
+                    os.system("sudo apt-get autoremove -y")
+                elif not ubuntu:
+                    install()
+        else:
+            print("Insufficient disk space. Required 2 GB or more for full setup.")
+        start_server()
+    elif full:
+        print("A full setup requires an internet connection. You may continue without the full option if the necessary resources are installed.")
+    else:
+        start_server()
+
+if sys.version_info.major == 3 and sys.version_info.minor >= 4:
+    if sys.platform == "linux":
+        env = platform.freedesktop_os_release().get("ID").lower()
+        os.system("sudo kill -9 $(sudo lsof -t -i:4000) 2>/dev/null")
+        if env == "ubuntu":
+            ubuntu = True
+            if os.getenv("GITHUB_ACTIONS") == "true":
+                githubactions = True
+            start_setup()
+        else:
+            print("Setup only works on Ubuntu Linux distribution.")
+    elif sys.platform == "win32":
+        env = platform.system().lower()
+        start_setup()
+    else:
+        print("Setup only works on Windows and Ubuntu OS.")
+else:
+    print("Python 3.4 or later is needed for setup.")
